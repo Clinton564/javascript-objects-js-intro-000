0f7e3823533a6d4b1374075a04fbd933e44d6aed
glorious-communicator-0033 <clinton.weaver@gmail.com>
2020-10-20 23:20:21 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -1,7 +1,7 @@
 var playlist = {singer: "singing"};
 
 function updatePlaylist(playlist, artistName, songTitle) {
-  return playlist[artistName]: songTitle
+  return playlist[artistName] = songTitle
 }
 
 function removeFromPlaylist(playlist, artistName) {
